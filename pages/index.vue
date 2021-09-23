91069552b3dc4fdc547463e4281c191df87abcc6
Fabrice NYONATO <nykorefa@gmail.com>
2021-09-23 05:26:41 +0000

    v0

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black">
+      <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="h-72 w-72">
+        <path id="blob" d="M445,292.5Q468,335,423,351Q378,367,352.5,386Q327,405,299,440.5Q271,476,236.5,448Q202,420,152.5,432.5Q103,445,93,399Q83,353,56,322Q29,291,55,254.5Q81,218,91.5,188.5Q102,159,114,122.5Q126,86,166,90Q206,94,238,64Q270,34,307.5,46Q345,58,376.5,82Q408,106,416,144Q424,182,423,216Q422,250,445,292.5Z" fill="#B53471"></path>
+    </svg>
+  </div>
+</template>
